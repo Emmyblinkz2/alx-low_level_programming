7e16cd170638199f68cff3fbc0ae3d4df03a0835
Emmanuel Ejemuta <emmanuelejemuta@gmail.com>
2022-04-08 19:21:21 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Return: Always 0 (success/correct)
+ * 
+ * Return: Always 0 (success)
  */
 int main(void)
-
 {
 char alphabet;
 
-for (alphabet = 'a'; alphabet <= "z"; alphabet++)
-{
+for (alphabet == 'a'; alphabet <= 'z'; alphabet++)
 putchar(alphabet);
-}
 
-putchar('\n');
-
+putchar ('\n');
+           
 return (0);
-
 }
