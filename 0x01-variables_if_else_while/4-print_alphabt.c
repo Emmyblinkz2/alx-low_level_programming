c1b35eb34429c4d3b113bae0ac4cd045da01e81a
Emmanuel Ejemuta <emmanuelejemuta@gmail.com>
2022-04-10 15:16:15 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,23 +5,20 @@
  * Return: Always 0 (success/correct)
  */
 int main(void)
-
 {
 char alphabet;
-
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
 if (alphabet == 'q')
 continue;
 
 else if (alphabet == 'e')
 continue;
-}
 
 putchar(alphabet);
-
+}
 putchar('\n');
 
 return (0);
-
 }
