6ac9a46e079048417eb8c7372bdbcede3f4f98f6
Emmanuel Ejemuta <emmanuelejemuta@gmail.com>
2022-04-11 05:57:00 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,3 @@
-
-
-
-
-
 #include <stdio.h>
 /**
  * main - Entry point
@@ -15,11 +10,15 @@ int main(void)
 char lower_case;
 
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+{
 if (lower_case == 'e')
 continue;
+
 else if (lower_case == 'q')
 continue;
+
 putchar(lower_case);
+}
 
 putchar ('\n');
 
