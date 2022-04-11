677e2820b0df3b7ed40487c4c54eb41f56a20ddb
Emmanuel Ejemuta <emmanuelejemuta@gmail.com>
2022-04-11 03:44:17 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+*/
+int main(void)
+{
+printf ("size of a char : I'd byte(s)\n", sizeof (char);
+printf ("size of a int : I'd byte(s)\n", sizeof (int);
+printf ("size of a long int : I'd byte(s)\n", sizeof (long int);
+printf ("size of a long long int : I'd byte(s)\n", sizeof (long long int);
+printf ("size of a float : I'd byte(s)\n", sizeof (float);
+return (0);
+}
