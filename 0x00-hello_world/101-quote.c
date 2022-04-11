c149022864c606cd95e49b3bd434c25c6f9ac94b
Emmanuel Ejemuta <emmanuelejemuta@gmail.com>
2022-04-11 06:13:13 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
- * Return: Always 0 (success)
+ * My hello, print task
+ * Return: return 1 after program execution
  */
 int main(void)
 {
-write (1,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+write("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
 return (1);
 }
