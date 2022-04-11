7d6bd3cc5c707e5edf392b6ff6eae601495aee04
Emmanuel Ejemuta <emmanuelejemuta@gmail.com>
2022-04-11 03:49:21 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-printf ("size of a char : I'd byte(s)\n", sizeof (char);
-printf ("size of a int : I'd byte(s)\n", sizeof (int);
-printf ("size of a long int : I'd byte(s)\n", sizeof (long int);
-printf ("size of a long long int : I'd byte(s)\n", sizeof (long long int);
-printf ("size of a float : I'd byte(s)\n", sizeof (float);
+printf ("size of a char : %d byte(s)\n", sizeof (char));
+printf ("size of a int : %d byte(s)\n", sizeof (int));
+printf ("size of a long int : %d byte(s)\n", sizeof (long int));
+printf ("size of a long long int : %d byte(s)\n", sizeof (long long int));
+printf ("size of a float : %d byte(s)\n", sizeof (float));
 return (0);
 }
