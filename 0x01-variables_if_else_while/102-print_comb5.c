99fcb68a73798daab4ed12dd9525d740232183f9
Emmanuel Ejemuta <emmanuelejemuta@gmail.com>
2022-04-12 02:14:10 -0700

    tasks 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (success/correct)
  */
 int main(void)
-{
-int i, j;
-for (i = 0; i < 100; i++)
 
-for (j = 0; j < 100; j++)
 {
-if (i < j)
+int first_nums;
+int second_nums;
+
+for (first_nums = 0; first_nums < 100 ; first_nums++)
 {
-putchar((i / 10) + 48);
-putchar((i % 10) + 48);
-putchar(' ');
-putchar((j / 10) + 48);
-putchar((j % 10) + 48);
-if (i != 98 || j != 99)
+for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
 {
-putchar('\n');
-putchar(' ');
+
+putchar (first_nums / 10 + '0');
+putchar (first_nums % 10 + '0');
+
+putchar (' ');
+
+putchar (second_nums / 10 + '0');
+putchar (second_nums % 10 + '0');
+
+if (first_nums == 98 && second_nums == 99)
+{
+break;
 }
+
+putchar(',');
+putchar (' ');
+
 }
+
 }
+
 putchar('\n');
 
 return (0);
