88929d3a98f127ebd753f1626ee1d3f53bc5eafb
Emmanuel Ejemuta <emmanuelejemuta@gmail.com>
2022-04-11 20:23:45 -0700

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,11 +9,11 @@ int main(void)
 {
 int i, j;
 for (i = 0; i < 100; i++)
-{
+
 for (j = 0; j < 100; j++)
-{
+
 if (i < j)
-{
+
 putchar((i / 10) + 48);
 putchar((i % 10) + 48);
 putchar(' ');
@@ -23,9 +23,6 @@ if (i != 98 || j != 99)
 {
 putchar('\n');
 putchar(' ');
-}
-}
-}
 }
 putchar('\n');
 return (0);
