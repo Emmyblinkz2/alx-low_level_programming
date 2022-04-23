b0a53cacf8dbbcdeb729a71d6c39d8f5e55b80de
Emmanuel Ejemuta <emmanuelejemuta@gmail.com>
2022-04-23 12:41:24 -0700

    task

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,15 +1,39 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * rot13 - encodes a string into rot13
- * @s: string to encode
- *
- * Return: address of s
- */
-char *rot13(char *s)
-{
-	int i, j;
-	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	for (i = 0; *(s + i)
+/**
+ * print_number - print integers
+ * @n: integer value.
+ *
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	unsigned int a, b, count, tmp, pow;
+
+	a = n;
+	pow = b = 1;
+
+	if (n < 0)
+	{
+		a *= -1;
+		_putchar('-');
+	}
+
+	tmp = a;
+
+	while (tmp > 9)
+	{
+		b++;
+		tmp /= 10;
+	}
+
+	for (count = 1; count < b; count++)
+		pow *= 10;
+
+	while (pow > 1)
+	{
+		_putchar((a / pow) % 10 + '0');
+		pow = pow / 10;
+	}
+	_putchar(a % 10 + '0');
+}
