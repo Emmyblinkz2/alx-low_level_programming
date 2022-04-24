8557c942b5e09031c7f96b81773fd4e3f99b7b98
Emmanuel Ejemuta <emmanuelejemuta@gmail.com>
2022-04-24 09:01:38 -0700

    task

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,25 +5,25 @@
  * Return: the pointer to dest.
  */
 
-char *rot13(char *);
+char *rot13(char *)
 {
 
-	int count = 0, i;
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+        int count = 0, i;
+        char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (*(s + count) != '\0')
-	{
-		for (i = 0; i < 52; i++)
-		{
-			if (*(s + count) == alphabet[i])
-			{
-				*(s + count) = rot13[i];
-				break;
-			}
-		}
-		count++;
-	}
+        while (*(s + count) != '\0')
+        {
+                for (i = 0; i < 52; i++)
+                {
+                        if (*(s + count) == alphabet[i])
+                        {
+                                *(s + count) = rot13[i];
+                                break;
+                        }
+                }
+                count++;
+        }
 
-	return (s);
+        return (s);
 }
