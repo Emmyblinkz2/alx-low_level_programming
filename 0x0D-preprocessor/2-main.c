656f6dad18719dc3b26d8b179a7fff3110275af4
Emmanuel Ejemuta <emmanuelejemuta@gmail.com>
2022-05-11 12:32:00 -0700

    tasks

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -10,13 +10,3 @@ int main(void)
 	printf("%s\n", __FILE__);
 	return (0);
 }
-
-
-3-function_like_macro.h:
-
-#ifndef _FUNCTION_LIKE_MACRO_
-#define _FUNCTION_LIKE_MACRO_
-
-#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
-
-#endif
